c04aac2771787b1147a5eb286d7765a2fce2978b
Khadijat <muhammadkhadijato@gmail.com>
2023-12-03 12:54:15 +0100

    first commit- Dom

---
+++ index.js
@@ -0,0 +1,21 @@
+// What is DOM?
+/*Document Object Model is a built in object is use to manipulate content, 
+style and structure in a project. it is one of the most unique and useful tools in js. 
+Anytime a website perform an action, submit form, error banner, open a link etc*/
+
+
+// Methods of Select Elements in the DOM
+
+/* DOM MANIPULATION
+//GetElementById()
+//GetElementByClassName()
+//GetElementByTagName()
+//querySelector()
+//querySelectorAll()*/
+
+
+
+
+// How to traverse the DOM
+// How to manipulate the DOM
+// Event Handling
