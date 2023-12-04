3dcc77ef4a04147c27596767817de7ec1871c4f7
Khadijat <muhammadkhadijato@gmail.com>
2023-12-04 09:45:08 +0100

    commit-5

---
+++ script.js
@@ -0,0 +1,26 @@
+//DOM MANIPULATION
+
+//GetElementById()
+const title = document.getElementById('main-heading');
+console.log(title);
+
+
+//GetElementByClassName()
+// const listItems = document.getElementsByClassName('list-items');
+// console.log(listItems);
+
+//GetElementByTagName()
+
+const listItems = document.getElementsByTagName('li');
+console.log(listItems);
+
+//querySelector()
+const container = document.querySelector('div');
+console.log(container);
+
+const li = document.querySelector('div');
+console.log(li);
+
+//querySelectorAll()
+const liS = document.querySelectorAll('li');
+console.log(liS);
