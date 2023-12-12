82fd96913b70a9199cd60a573c15e4c21176f5e1
Khadijat <muhammadkhadijato@gmail.com>
2023-12-12 18:57:52 +0100

    project 2- commit 5

---
+++ scriptp2.js
@@ -0,0 +1,35 @@
+//DOM MANIPULATION
+
+//EVENT LISTENERS
+
+//element.eventlistener('click', function)
+
+const boxOne = document.querySelector('.two');
+
+function changeBackground () {
+    boxOne.style.backgroundColor = "red"
+}
+
+function color() {
+    boxOne.style.backgroundColor = "#190280"
+    boxOne.style.color = "#44f0d9"
+
+}
+
+boxOne.addEventListener('mouseover', changeBackground);
+boxOne.addEventListener('mouseout', color);
+
+
+const btn3 = document.querySelector('.btn-3');
+
+function popUp() {
+    alert("You are doing well")
+ }
+
+ function changeBckgrdClr() {
+    btn3.style.backgroundColor = "white";
+ }
+
+
+btn3.addEventListener('click', popUp);
+btn3.addEventListener('mouseover', changeBckgrdClr);
