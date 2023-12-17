ecd1e7497ae7a28226e7990f113143fb6a9ee0c5
Khadijat <muhammadkhadijato@gmail.com>
2023-12-17 23:36:36 +0100

    project 5 updated

--- File-proj-5/scriptp5.js
+++ File-proj-5/scriptp5.js
@@ -14,3 +14,18 @@ function change() {
 list.addEventListener('click', change);
 
 
+document.querySelector('#word').addEventListener('click', function () {
+    console.log('Word clicked');
+});
+document.querySelector('#powerpoint').addEventListener('click', function () {
+    console.log('Powerpoint clicked');
+});
+document.querySelector('#excel').addEventListener('click', function () {
+    console.log('Excel clicked');
+});
+document.querySelector('#paint').addEventListener('click', function () {
+    console.log('Paint clicked');
+});
+document.querySelector('#access').addEventListener('click', function () {
+    console.log('Access clicked');
+});
