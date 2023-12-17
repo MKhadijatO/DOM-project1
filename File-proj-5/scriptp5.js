6ef279fdb031816fadea0b0dc8ef1bd77b135fc8
Khadijat <muhammadkhadijato@gmail.com>
2023-12-18 00:08:45 +0100

    project 5 updated

--- File-proj-5/scriptp5.js
+++ File-proj-5/scriptp5.js
@@ -14,7 +14,7 @@
 // list.addEventListener('click', change);
 
 
-document.querySelector('#word').addEventListener('click', function (e) {
+/*document.querySelector('#word').addEventListener('click', function (e) {
     console.log('Word clicked');
 
     const target = e.target;
@@ -72,4 +72,25 @@ document.querySelector('#access').addEventListener('click', function (e) {
         target.style.color = "black";
         target.style.fontWeight = "bold";
     } 
+});*/
+
+
+document.querySelector('#office').addEventListener
+('click', function (e) {
+    console.log( e.target.getAttribute('id') + ' is clicked');
+    
+    const target = e.target;
+    if(target.matches('li')) {
+        target.style.backgroundColor = "white";
+        target.style.color = "black";
+        target.style.fontWeight = "bold";
+    }
 });
+
+const office = document.querySelector('#office');
+const newOffice = document.createElement('li');
+
+newOffice.innerHTML = 'Docment';
+newOffice.setAttribute('id', 'office');
+
+office.appendChild(newOffice);
