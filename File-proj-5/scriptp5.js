598ece261a20f8930200f2549b02920d73c68962
Khadijat <muhammadkhadijato@gmail.com>
2023-12-17 23:27:55 +0100

    project 5 first commit: updated

--- File-proj-5/scriptp5.js
+++ File-proj-5/scriptp5.js
@@ -2,4 +2,15 @@
 
 // EVENT DELEGATION
 
-//
+const list = document.querySelector('.items');
+
+function change() {
+    list.style.backgroundColor = "white";
+    list.style.color = "black";
+    list.style.fontWeight = "bold";
+};
+
+
+list.addEventListener('click', change);
+
+
